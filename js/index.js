10460d6492bd0af4e6f4b795b8a8126e03ca4213
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-08-06 14:48:13 +0200

    new JS functionalities added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,52 @@
+//Menú hamburguesa
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-const d = new Date();
-document.getElementById("fecha").innerHTML = d;
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+  name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  phone: /^\d{7,14}$/, // 7 a 14 numeros.
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "name":
+      if (expresiones.name.test(e.target.value)) {
+        document
+          .getElementById("grupo_name")
+          .classList.add("contact-us-form_grupo-correcto");
+        document
+          .getElementById("grupo_name")
+          .classList.remove("contact-us-form_grupo-incorrecto");
+      } else {
+        document
+          .getElementById("grupo_name")
+          .classList.add("contact-us-form_grupo-incorrecto");
+        document
+          .getElementById("grupo_name")
+          .classList.remove("contact-us-form_grupo-correcto");
+      }
+      break;
+    case "email-contact":
+      break;
+    case "phone":
+      break;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
